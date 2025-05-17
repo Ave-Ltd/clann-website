2eb62f59aa04b17f249dd8bc27041534cd035bc6
Your NameMr <you@example.com>
2025-05-17 19:34:29 +0100

    Update: Footer to use logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,162 +7,56 @@ export const Footer = () => {
       <hr className="w-11/12 mx-auto" />
 
       <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+        {/* Full-size logo like in navbar */}
         <div className="col-span-full xl:col-span-2">
           <a
-            rel="noreferrer noopener"
             href="/"
-            className="font-bold text-xl flex"
+            className="font-bold text-xl flex items-center space-x-2"
+            rel="noreferrer noopener"
           >
-            <LogoIcon />
-            Clubs Hub
+            <span className="h-10 w-auto flex items-center">
+              <LogoIcon className="h-10 w-auto" />
+            </span>
           </a>
         </div>
 
+        {/* Follow Us */}
         <div className="flex flex-col gap-2">
           <h3 className="font-bold text-lg">Follow Us</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Github
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Twitter
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Dribbble
-            </a>
-          </div>
+          <a href="#" className="opacity-60 hover:opacity-100">Github</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Twitter</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Dribbble</a>
         </div>
 
+        {/* Platforms */}
         <div className="flex flex-col gap-2">
           <h3 className="font-bold text-lg">Platforms</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Web
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Mobile
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Desktop
-            </a>
-          </div>
+          <a href="#" className="opacity-60 hover:opacity-100">Web</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Mobile</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Desktop</a>
         </div>
 
+        {/* About */}
         <div className="flex flex-col gap-2">
           <h3 className="font-bold text-lg">About</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Features
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Pricing
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              FAQ
-            </a>
-          </div>
-
-          <div>
-            <Link
-              to="/credits"
-              className="opacity-60 hover:opacity-100"
-            >
-              Credits
-            </Link>
-          </div>
+          <a href="#" className="opacity-60 hover:opacity-100">Features</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Pricing</a>
+          <a href="#" className="opacity-60 hover:opacity-100">FAQ</a>
+          <Link to="/credits" className="opacity-60 hover:opacity-100">Credits</Link>
         </div>
 
+        {/* Community */}
         <div className="flex flex-col gap-2">
           <h3 className="font-bold text-lg">Community</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Youtube
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Discord
-            </a>
-          </div>
-
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="#"
-              className="opacity-60 hover:opacity-100"
-            >
-              Twitch
-            </a>
-          </div>
+          <a href="#" className="opacity-60 hover:opacity-100">Youtube</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Discord</a>
+          <a href="#" className="opacity-60 hover:opacity-100">Twitch</a>
         </div>
       </section>
 
       <section className="container pb-14 text-center">
         <h3>
-          &copy; 2024 Landing page made by{" "}
+          &copy; Created by{" "}
           <a
             rel="noreferrer noopener"
             target="_blank"
